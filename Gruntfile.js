afbe9bbf0cb87b8f0d14f1a2a670011222bd5b16
James Allardice <jim.allardice@gmail.com>
2013-07-10 08:32:37 +0100

    Builds for dev/production

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,6 +40,28 @@ module.exports = function (grunt) {
                 }
             }
         },
+        clean: {
+            all: [
+                "dist"
+            ],
+            max: [
+                "dist/emoji.css"
+            ]
+        },
+        copy: {
+            max: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            "lib/emoji.js"
+                        ],
+                        dest: "dist/"
+                    }
+                ]
+            }
+        },
         uglify: {
             browser: {
                 files: {
@@ -83,14 +105,25 @@ module.exports = function (grunt) {
 
     grunt.registerTask("default", [
         "jshint",
+        "clean:all",
         "montage",
         "uglify",
-        "cssmin"
+        "cssmin",
+        "clean:max"
+    ]);
+
+    grunt.registerTask("dev", [
+        "jshint",
+        "clean:all",
+        "montage",
+        "copy"
     ]);
 
     grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-montage");
 
 };
